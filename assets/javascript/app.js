e8d31994f1944cb661550bce389ae7d779d01435
Casey Rubel <caseyrubel@Caseys-MacBook-Air.local>
2017-05-24 16:56:22 -0700

    working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,54 @@
 
 
-var buttons = [];
+var buttons = ["bird", "lion"];
 
-$("button").on("click", function() {
-      // Grabbing and storing the data-animal property value from the button
-  var animal = $(this).attr("data-animal");
-  // Constructing a queryURL using the animal name
+function displayAnimalInfo() {
+  $("#giphy-view").empty();
+  var animal = $(this).attr("data-name");
+  console.log(animal);
   var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
     animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-  // Performing an AJAX request with the queryURL
   $.ajax({
       url: queryURL,
       method: "GET"
     })
-    // After data comes back from the request
     .done(function(response) {
       console.log(queryURL);
       console.log(response);
-      // storing the data from the AJAX request in the results variable
       var results = response.data;
-      // Looping through each result item
       for (var i = 0; i < results.length; i++) {
-        // Creating and storing a div tag
         var animalDiv = $("<div>");
-        // Creating a paragraph tag with the result item's rating
         var p = $("<p>").text("Rating: " + results[i].rating);
-        // Creating and storing an image tag
         var animalImage = $("<img>");
-        // Setting the src attribute of the image to a property pulled off the result item
         animalImage.attr("src", results[i].images.fixed_height.url);
-        // Appending the paragraph and image tag to the animalDiv
         animalDiv.append(p);
         animalDiv.append(animalImage);
-        // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
         $("#giphy-view").prepend(animalDiv);
       }
-  	});
-});
+	});
+}
 
-$("#submit").click(function(event){
-	event.preventDefault();
-	animal = $("#gif-input").val();
-	a = $("<button class='button'></button>");
-	$("#buttons-view").append(a);
-	a.html(animal);
-})
+function renderButtons() {
+        $("#buttons-view").empty();
+        for (var i = 0; i < buttons.length; i++) {
+          var a = $("<button>");
+          a.addClass("button");
+          a.addClass("btn btn-default");
+          a.attr("data-name", buttons[i]);
+          a.text(buttons[i]);
+          $("#buttons-view").append(a);
+        }
+      }
+
+$("#add-giphy").on("click", function(event) {
+        event.preventDefault();
+        var animal = $("#animal-input").val().trim();
+        buttons.push(animal);
+        renderButtons();
+      });
+      $(document).on("click", ".button", displayAnimalInfo);
+      renderButtons();
+
 
 
 
